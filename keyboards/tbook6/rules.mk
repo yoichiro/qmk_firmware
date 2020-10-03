37a2657bd2612de4f86a58b8a6533274871c2465
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-10-03 21:41:01 +0900

    Add OLED.

--- keyboards/tbook6/rules.mk
+++ keyboards/tbook6/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -21,5 +21,5 @@ RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = yes           # Audio output
 
-# OLED_DRIVER_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
 SPLIT_KEYBOARD = yes
