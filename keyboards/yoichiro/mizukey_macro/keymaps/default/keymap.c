c58e149670cd144108907eefb29d96c7188aa4c2
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-10-21 19:41:31 +0900

    Update the keymap.

--- keyboards/yoichiro/mizukey_macro/keymaps/default/keymap.c
+++ keyboards/yoichiro/mizukey_macro/keymaps/default/keymap.c
@@ -14,33 +14,55 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "song_list.h"
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _BASE
-//    _FN
+    _BASE,
+    _BROWSER
 };
 
 // Defines the keycodes used by our macros in process_record_user
-//enum custom_keycodes {
-//    QMKBEST = SAFE_RANGE,
-//    QMKURL
-//};
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
-    [_BASE] = LAYOUT(
-        KC_A,    KC_B,    KC_C,    KC_D, \
-        KC_E,    KC_F,    KC_G,    KC_H, \
-        KC_I,    KC_J,    KC_K           \
-    )
-//    [_FN] = LAYOUT(
-//        QMKBEST, QMKURL,  _______,
-//            RESET,    XXXXXXX
-//    )
+enum custom_keycodes {
+  BASE = SAFE_RANGE,
+  BROWSER
 };
 
-//bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT(
+        KC_PGUP,      KC_HOME,    KC_UP,      KC_END, \
+        KC_PGDN,      KC_LEFT,    KC_DOWN,    KC_RGHT, \
+        MO(_BROWSER), KC_NO,      KC_ESC \
+    ),
+    [_BROWSER] = LAYOUT(
+        KC_WH_U,      KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2, \
+        KC_WH_D,      KC_MS_L,    KC_MS_D,    KC_MS_R, \
+        KC_TRNS,      _______,    _______ \
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//  if (record->event.pressed) {
+//    float my_song[][2] = SONG(GUITAR_SOUND);
+//    PLAY_SONG(my_song);
+//  }
+//  switch (keycode) {
+//    case BASE:
+//      if (record->event.pressed) {
+//        set_single_persistent_default_layer(_BASE);
+//      }
+//      return false;
+//      break;
+//    case BROWSER:
+//      if (record->event.pressed) {
+//        layer_on(_BROWSER);
+//      } else {
+//        layer_off(_BROWSER);
+//      }
+//      return false;
+//      break;
+//  }
+  return true;
 //    switch (keycode) {
 //        case QMKBEST:
 //            if (record->event.pressed) {
@@ -60,4 +82,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //            break;
 //    }
 //    return true;
-//}
+}
