be82f13fd083b75333a9fb33049f6dc2fef75ab7
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-11-03 11:22:30 +0900

    Update firmware for Macro.

--- keyboards/yoichiro/mizukey_macro/config.h
+++ keyboards/yoichiro/mizukey_macro/config.h
@@ -71,9 +71,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-  #define RGBLIGHT_LAYERS
+//  #define RGBLIGHT_LAYERS
 /*== all animations enable ==*/
-//  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
 //#    define RGBLIGHT_EFFECT_BREATHING
 //  #define RGBLIGHT_EFFECT_RAINBOW_MOOD
--- keyboards/yoichiro/mizukey_macro/keymaps/default/keymap.c
+++ keyboards/yoichiro/mizukey_macro/keymaps/default/keymap.c
@@ -22,7 +22,8 @@
 enum layer_names {
     _CURSOR,
     _BROWSER,
-    _NUMBERS
+    _NUMBERS,
+    _ADJUST
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -46,44 +47,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT(
         KC_1,         KC_2,       KC_3,        KC_4, \
         KC_5,         KC_6,       KC_7,        KC_8, \
-        KC_9,         KC_0,       DF(_CURSOR) \
+        KC_9,         KC_0,       DF(_ADJUST) \
+    ),
+    [_ADJUST] = LAYOUT(
+        RGB_TOG,      RGB_MOD,    RGB_VAI,     RGB_VAD, \
+        AU_TOG,       CK_TOGG,    MU_TOG,      MU_MOD, \
+        KC_NO,        KC_NO,      DF(_CURSOR) \
     )
 };
 
-const rgblight_segment_t PROGMEM rgb_cursor_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 3, HSV_RED}
-);
-const rgblight_segment_t PROGMEM rgb_browser_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 3, HSV_BLUE}
-);
-const rgblight_segment_t PROGMEM rgb_numbers_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 3, HSV_GREEN}
-);
-const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    rgb_cursor_layer,
-    rgb_browser_layer,
-    rgb_numbers_layer
-);
-
-void keyboard_post_init_user() {
-    rgblight_layers = rgb_layers;
-    debug_enable = true;
-}
-
-layer_state_t default_layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
-    rgblight_set_layer_state(2, layer_state_cmp(state, 2));
-    return state;
-}
-
+//const rgblight_segment_t PROGMEM rgb_cursor_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//    {0, 3, HSV_RED}
+//);
+//const rgblight_segment_t PROGMEM rgb_browser_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//    {0, 3, HSV_BLUE}
+//);
+//const rgblight_segment_t PROGMEM rgb_numbers_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+//    {0, 3, HSV_GREEN}
+//);
+//const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+//    rgb_cursor_layer,
+//    rgb_browser_layer,
+//    rgb_numbers_layer
+//);
+//
+//void keyboard_post_init_user() {
+//    rgblight_layers = rgb_layers;
+//    debug_enable = true;
+//}
+//
+//layer_state_t default_layer_state_set_user(layer_state_t state) {
+//    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
+//    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
+//    rgblight_set_layer_state(2, layer_state_cmp(state, 2));
+//    return state;
+//}
+//
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    float my_song[][2] = SONG(E__NOTE(_A6));
+    float next_song[][2] = SONG(E__NOTE(_A6));
+    float back_song[][2] = SONG(H__NOTE(_D2));
     switch (keycode) {
         case DF(_CURSOR):
         case DF(_BROWSER):
         case DF(_NUMBERS):
-            PLAY_SONG(my_song);
+            PLAY_SONG(next_song);
+            break;
+        case DF(_ADJUST):
+            PLAY_SONG(back_song);
             break;
     }
     return true;
