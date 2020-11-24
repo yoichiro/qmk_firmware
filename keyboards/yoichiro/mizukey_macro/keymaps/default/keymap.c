f58135f3a9ca34915bf2180210ca8c2c346f284f
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-11-24 09:11:38 +0900

    Change some keycodes.

--- keyboards/yoichiro/mizukey_macro/keymaps/default/keymap.c
+++ keyboards/yoichiro/mizukey_macro/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_CURSOR] = LAYOUT(
         KC_PGUP,      KC_HOME,    KC_UP,       KC_END, \
         KC_PGDN,      KC_LEFT,    KC_DOWN,     KC_RGHT, \
-        KC_LSFT,      KC_ESC,     DF(_ADJUST) \
+        KC_ENT,       KC_BSPC,    DF(_ADJUST) \
     ),
     [_ADJUST] = LAYOUT(
         RGB_TOG,      RGB_MOD,    RGB_VAI,     RGB_VAD, \
