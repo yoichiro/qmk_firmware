b0d24dd56e95f7cbab934cd389d962c7a84fb922
Yoichiro Tanaka <yoichiro6642@gmail.com>
2020-12-01 10:30:08 +0900

    Enable OLED module.

--- keyboards/yoichiro/lunakey_mini/keymaps/yoichiro/rules.mk
+++ keyboards/yoichiro/lunakey_mini/keymaps/yoichiro/rules.mk
@@ -1,5 +1,5 @@
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = yes           # Audio output
-OLED_DRIVER_ENABLE = no      # OLED Display Driver
+OLED_DRIVER_ENABLE = yes     # OLED Display Driver
 LTO_ENABLE = yes             # CFLAGS=flto
 VIA_ENABLE = no              # Enable VIA support
